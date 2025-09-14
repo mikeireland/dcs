fc8c4a89f855e55fd659fe2f7ad4ac6ebb9cba86
ataras2 <ataras2@gmail.com>
2025-09-13 22:32:19 -0300

    verbose

--- mcs_client/mcs_client.py
+++ mcs_client/mcs_client.py
@@ -298,7 +298,8 @@ class MCSClient:
             logging.info("No msgs found, waiting...")
             return
         body = self.ESO_format(all_params)
-        logging.info(f"pushing: {str(body)[:20]}]...")
+        # logging.info(f"pushing: {str(body)[:20]}]...")
+        logging.info(f"pushing: {str(body)}...")
         try:
             ok, msg = self._send(body)
             if not ok:
